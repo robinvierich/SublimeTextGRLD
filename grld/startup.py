3df6513e7bc213cb1890767317bc0629aa664d08
robin <robin.v@capybaragames.com>
2017-06-06 13:02:32 -0400

    updated startup file (WIP)

--- grld/startup.py
+++ grld/startup.py
@@ -3,8 +3,4 @@ from grld.command_worker import CommandWorker
 
 
 def main():
-    net_worker = NetWorker()
-    net_worker.start()
-
-    command_worker = CommandWorker()
-    command_worker.start()
+    
